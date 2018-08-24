51b0330623777d5c2cfa00549dd807e8a7ecca1b
time-killer-games <samuelvenable@hotmail.com>
2018-08-23 22:57:07 -0400

    Delete windowgetcaption.m

--- DialogModule.dylib/DialogModule/windowgetcaption.m
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-const char *cocoa_window_get_caption(void *window_handle)
-{
-    NSWindow *window;
-    window = (NSWindow *)window_handle;
-    
-    return [[window title] UTF8String];
-}
